35be81981501e87812c1d39ca333444177bac74b
QBallardUMBC <uw94057@umbc.edu>
2026-01-27 21:30:10 -0500

    Testing diff years

--- data/raw/OPENAQ.py
+++ data/raw/OPENAQ.py
@@ -95,6 +95,23 @@ def get_locations(loc_coord: list[float]):
 
 all_locations = get_locations(BALTIMORE_COUNTY_CENTER)
 
+def get_specific_loc(location: str):
+    for loc in all_locations:
+        if loc.name == location:
+            return loc
+
+def get_location_details():
+    location_list = []
+    for loc in all_locations:
+        location_dict = {
+            "id": loc.id,
+            "name": loc.name,
+            "timezone": loc.timezone,
+            "country": loc.country.name       
+        }
+        location_list.append(location_dict)
+    return location_list
+
 def get_data_lvls(data_type: str, date_from: str, date_to: str):
     rows = []
 
@@ -137,12 +154,12 @@ def get_data_lvls_agg(data_type: str, aggregation: str, date_from: str, date_to:
         for s in (loc.sensors or []):
             if s.parameter and s.parameter.name == data_type:
 
-                hourly = fetch_all(
-                    f"{BASE_URL}/sensors/{s.id}{aggregation}",
-                    {"datetime_from":date_from, "datetime_to":date_to}
+                agg = fetch_all(
+                    url=f"{BASE_URL}/sensors/{s.id}{aggregation}",
+                    params={"date_from":date_from, "date_to":date_to}
                 )
 
-                for r in hourly:
+                for r in agg:
                     
                     data = {
                         "sensor_id": s.id,
@@ -158,17 +175,76 @@ def get_data_lvls_agg(data_type: str, aggregation: str, date_from: str, date_to:
                         "q75": r["summary"]["q75"],
                         "q98": r["summary"]["q98"],
                         "max": r["summary"]["max"],
-                        "avg": r["summary"]["avg"]
+                        "avg": r["summary"]["avg"],
+                        "sd": r["summary"]["sd"],
+                        "date_from": r["coverage"]["datetimeFrom"]["local"],
+                        "date_to": r["coverage"]["datetimeTo"]["local"]
                     }
 
                     rows.append(data)
 
     return rows
 
-rows = get_data_lvls_agg(PM25, YEARLY, "2018-01-01", "2019-01-01")
-df = pd.DataFrame(rows)
-print(df.head())
-print("rows:", len(df))
-df.to_excel("pm25year.xlsx", index=False)
+def get_data_lvls_agg_loc(data_type: str, loc, aggregation: str, date_from: str, date_to: str):
+    
+    rows = []
+    for s in (loc.sensors or []):
+        if s.parameter and s.parameter.name == data_type:
 
+            agg = fetch_all(
+                f"{BASE_URL}/sensors/{s.id}{aggregation}",
+                {"date_from":date_from, "date_to":date_to}
+            )
+
+            for r in agg:
+                    
+                data = {
+                    "sensor_id": s.id,
+                    "location_id": loc.id,
+                    "location_name": loc.name,
+                    "parameter": s.parameter.name,
+                    "parameter_units": s.parameter.units,
+                    "value": r["value"],
+                    "min": r["summary"]["q02"],
+                    "q02": r["summary"]["q02"],
+                    "q25": r["summary"]["q25"],
+                    "median": r["summary"]["median"],
+                    "q75": r["summary"]["q75"],
+                    "q98": r["summary"]["q98"],
+                    "max": r["summary"]["max"],
+                    "avg": r["summary"]["avg"],
+                    "sd": r["summary"]["sd"],
+                    "date_from": r["coverage"]["datetimeFrom"]["local"],
+                    "date_to": r["coverage"]["datetimeTo"]["local"]
+                }
+
+                rows.append(data)
+            time.sleep(1/60)
+
+    return rows
+
+def Padonia():
+    #Daily
+    for i in range(0, 26):
+        loc = get_specific_loc("Padonia")
+        rows = get_data_lvls_agg_loc(PM25, loc, DAILY, f"20{i:02d}-01-01", f"20{i+1:02d}-01-01")
+        df = pd.DataFrame(rows)
+        df.to_excel(f"padonia_pm25_daily_20{i:02d}.xlsx", index=False)
+    
+    #ANUALLY
+    for i in range(0, 26):
+        loc = get_specific_loc("Padonia")
+        rows = get_data_lvls_agg_loc(PM25, loc, YEARLY, f"20{i:02d}-01-01", f"20{i+1:02d}-01-01")
+        df = pd.DataFrame(rows)
+        df.to_excel(f"padonia_pm25_yearly_20{i:02d}.xlsx", index=False)
+
+#loc = get_specific_loc("Padonia")
+#rows = get_data_lvls_agg_loc(PM25, loc, DAILY, "2024-01-01", "2025-01-01")
+#print(pd.DataFrame(location_list))
+
+for i in range(0, 26):
+    rows = get_data_lvls_agg(O3, DAILY, f"20{i:02d}-01-01", f"20{i+1:02d}-01-01")
+    df = pd.DataFrame(rows)
+    df.to_excel("o3day.xlsx", index=False)
+#Padonia()
 client.close()
